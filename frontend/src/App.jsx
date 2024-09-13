e124b380265669bb87ed9956ed7fdde77761c15f
Aditya-Deshmukh9 <adityadesh937@gmail.com>
2024-09-13 17:57:16 +0530

    UI completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import LoginPage from "./pages/auth/login/LoginPage";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from "./components/common/RightPanel";
+import { Toaster } from "react-hot-toast";
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <Router>
+      <div className="flex max-w-6xl mx-auto">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/notifications" element={<NotificationPage />} />
+          <Route path="/profile/:username" element={<ProfilePage />} />
+        </Routes>
+        <RightPanel />
+      </div>
+      <Toaster />
+    </Router>
+  );
 }
 
 export default App;
